324e39e3c6dad53c1e453065d5577ca9519bbfa9
List <61047602+namest504@users.noreply.github.com>
2023-05-07 18:49:04 +0900

    [spring-stomp-chat] init

---
+++ spring-stomp-chat/src/main/java/com/list/springstomp/SpringstompApplication.java
@@ -0,0 +1,13 @@
+package com.list.springstomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstompApplication.class, args);
+	}
+
+}
