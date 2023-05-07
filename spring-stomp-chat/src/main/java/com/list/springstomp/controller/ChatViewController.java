9ee172c9264aa59a89edbac64c892c862a81337a
List <61047602+namest504@users.noreply.github.com>
2023-05-07 19:02:48 +0900

    [spring-stomp-chat] feat : REST API 분리

---
+++ spring-stomp-chat/src/main/java/com/list/springstomp/controller/ChatViewController.java
@@ -0,0 +1,24 @@
+package com.list.springstomp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ChatViewController {
+
+    // 채팅 리스트 화면
+    @GetMapping("/room")
+    public String rooms(Model model) {
+
+        return "/chat/room";
+    }
+
+    // 채팅방 입장 화면
+    @GetMapping("/room/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable String roomId) {
+        model.addAttribute("roomId", roomId);
+        return "/chat/roomdetail";
+    }
+}
