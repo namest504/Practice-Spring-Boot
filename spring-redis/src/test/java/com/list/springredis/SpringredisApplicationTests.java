ee65f6738374d5e2a676bd51db10cf908779a7c2
List <61047602+namest504@users.noreply.github.com>
2023-05-14 15:27:12 +0900

    [spring-redis] chore: init

---
+++ spring-redis/src/test/java/com/list/springredis/SpringredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.springredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
