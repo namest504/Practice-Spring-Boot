1b9e516e8d857c0cf460f7203d584efb076476f1
List <61047602+namest504@users.noreply.github.com>
2023-06-09 19:20:25 +0900

    [spring-webclient] feat : user 조회 API

--- spring-webclient/src/main/java/com/list/webclient/controller/WebClientController.java
+++ spring-webclient/src/main/java/com/list/webclient/controller/WebClientController.java
@@ -1,8 +1,11 @@
 package com.list.webclient.controller;
 
+import com.list.webclient.dto.User.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,4 +15,15 @@ public class WebClientController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/apitest")
+    public Mono<UserResponse> basicApiTest() {
+        return WebClient.create("https://jsonplaceholder.typicode.com")
+                .get()
+                .uri("/todos/1")
+                .retrieve()
+                .bodyToMono(UserResponse.class);
+    }
+
+
 }
